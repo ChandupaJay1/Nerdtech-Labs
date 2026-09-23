3c3081413eabbd1c1fb0e8d2073da5ca1a2d87eb
ChandupaJay1 <Chanudpajayalath20@gmail.com>
2026-09-23 16:01:47 +0530

    remove tag script 3

--- resources/views/blog-standard.blade.php
+++ resources/views/blog-standard.blade.php
@@ -4,7 +4,6 @@
 
 <!-- Mirrored from demo.egenslab.com/html/softconic/preview/blog-standard.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 31 Jan 2026 18:08:04 GMT -->
 <head>
-    <script type="text/javascript" src="https://app.secureprivacy.ai/script/6ab3a09414671a86a00afe22.js"></script>
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-7ZTFLHV8G5"></script>
     <script>
